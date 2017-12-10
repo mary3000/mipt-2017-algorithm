aba3d8142a5c8c6cff77ca78c199f0de28ad3ee1
mary <mary-fifa3000@mail.ru>
2017-12-10 16:09:32 +0300

    fix

--- task-4-A/biginteger.h
+++ task-4-A/biginteger.h
@@ -2,8 +2,9 @@
 // Created by User on 07.12.2017.
 //
 
-#include <utility>
 #include <vector>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -14,37 +15,73 @@ const int kDimension = 10;
 
 class BigInteger {
  public:
+  BigInteger() : number({0}), is_positive(true) {}
   BigInteger(vector<char> value, bool is_positive) : number(std::move(value)), is_positive(is_positive) {}
+  BigInteger(int input);
 
-  const BigInteger operator+(const BigInteger &other) const;
-  const BigInteger operator-(const BigInteger &other) const;
-  const BigInteger operator*(const BigInteger &other) const;
-  const BigInteger operator%(const BigInteger &other) const;
-  const BigInteger operator+=(const BigInteger &other) const;
-  const BigInteger operator-=(const BigInteger &other) const;
-  const BigInteger operator*=(const BigInteger &other) const;
-  const BigInteger operator/=(const BigInteger &other) const;
-  const BigInteger operator%=(const BigInteger &other) const;
-  const BigInteger operator-(void) const;
-  const BigInteger operator++(void);
-  const BigInteger operator++(int);
-  const BigInteger operator--(void);
-  const BigInteger operator--(int);
+  BigInteger operator+(const BigInteger &other) const;
+  BigInteger operator-(const BigInteger &other) const;
+  BigInteger operator*(const BigInteger &other) const;
+  BigInteger operator/(BigInteger other) const;
+  BigInteger operator%(const BigInteger &other) const;
+  BigInteger operator+=(const BigInteger &other);
+  BigInteger operator-=(const BigInteger &other);
+  BigInteger operator*=(const BigInteger &other);
+  BigInteger operator/=(const BigInteger &other);
+  BigInteger operator%=(const BigInteger &other);
+  BigInteger operator-() const;
+  BigInteger& operator++();
+  BigInteger operator++(int);
+  BigInteger& operator--();
+  BigInteger operator--(int);
+  bool operator==(const BigInteger &other) const;
+  bool operator!=(const BigInteger &other) const;
+  bool operator<(const BigInteger &other) const;
+  bool operator<=(const BigInteger &other) const;
+  bool operator>(const BigInteger &other) const;
+  bool operator>=(const BigInteger &other) const;
 
-  const BigInteger operator+(int other) const;
- //private:
+  explicit operator bool() const;
+  string toString() const;
+
+  //private:
   vector<char> number;
   bool is_positive;
 
-  const BigInteger positive_addition(const BigInteger &other) const;
-  const BigInteger positive_subtraction(const BigInteger &other) const;
+  friend ostream& operator<<(ostream& os,  const BigInteger &big_int);
+  friend istream& operator>>(istream& is, BigInteger &big_int);
+
+  friend BigInteger operator-(int first, const BigInteger &second);
+  friend BigInteger operator+(int first, const BigInteger &second);
+  friend BigInteger operator*(int first, const BigInteger &second);
+  friend BigInteger operator/(int first, const BigInteger &second);
+  friend BigInteger operator%(int first, const BigInteger &second);
+
+  BigInteger positive_addition(const BigInteger &other) const;
+  BigInteger positive_subtraction(const BigInteger &other) const;
+  int small_division(const BigInteger &other);
 };
 
-const BigInteger BigInteger::operator+(const BigInteger &other) const {
-  /*if (is_positive != other.is_positive) {
+BigInteger::BigInteger(int input) {
+  is_positive = input >= 0;
+  if (input < 0) {
+    input = -input;
+  }
+  if (input == 0) {
+    number.push_back(0);
+    return;
+  }
+  while (input != 0) {
+    number.push_back(input % kDimension);
+    input /= kDimension;
+  }
+}
+
+BigInteger BigInteger::operator+(const BigInteger &other) const {
+  if (is_positive != other.is_positive) {
     if (is_positive) {
       if (*this >= -other) {
-        positive_subtraction(other);
+        return positive_subtraction(other);
       }
       return -other.positive_subtraction(*this);
     }
@@ -53,22 +90,22 @@ const BigInteger BigInteger::operator+(const BigInteger &other) const {
     }
     return -positive_subtraction(other);
   }
-  return positive_addition(other);*/
+  return positive_addition(other);
 }
 
-const BigInteger BigInteger::positive_subtraction(const BigInteger &other) const {
+BigInteger BigInteger::positive_subtraction(const BigInteger &other) const {
   vector<char> new_number;
   bool remainder = false;
   int first, second;
   first = second = 0;
-  for (int i = 0; i < number.size(); i++) {
+  for (size_t i = 0; i < number.size(); i++) {
     first = number[i] - remainder;
     second = i < other.number.size() ? other.number[i] : 0;
-    new_number.push_back((kDimension + first - second) % 10);
+    new_number.push_back((kDimension + first - second) % kDimension);
     remainder = first < second;
   }
   unsigned int end = new_number.size();
-  while (end >= 0) {
+  while (end > 0) {
     end--;
     if (new_number[end] != 0) {
       break;
@@ -77,63 +114,268 @@ const BigInteger BigInteger::positive_subtraction(const BigInteger &other) const
   new_number.resize(end + 1);
   return BigInteger(new_number, true);
 }
-const BigInteger BigInteger::positive_addition(const BigInteger &other) const {
+
+BigInteger BigInteger::positive_addition(const BigInteger &other) const {
   vector<char> new_number;
-  int max_length = number.size() > other.number.size() ? number.size() : other.number.size();
+  size_t max_length = number.size() > other.number.size() ? number.size() : other.number.size();
   bool remainder = false;
   int first, second;
   first = second = 0;
-  for (int i = 0; i <= max_length; i++) {
+  for (size_t i = 0; i <= max_length; i++) {
     if (!remainder && i >= number.size() && i >= other.number.size()) {
       break;
     }
     first = i < number.size() ? number[i] : 0;
     second = i < other.number.size() ? other.number[i] : 0;
-    new_number.push_back((first + second + remainder) % 10);
-    remainder = (first + second + remainder) / 10;
+    new_number.push_back((first + second + remainder) % kDimension);
+    remainder = (first + second + remainder) / kDimension;
   }
   return BigInteger(new_number, is_positive);
 }
-const BigInteger BigInteger::operator-(const BigInteger &other) const {
-  return *this + BigInteger(other.number, false);
+
+BigInteger BigInteger::operator-(const BigInteger &other) const {
+  return *this + BigInteger(other.number, !other.is_positive);
 }
-const BigInteger BigInteger::operator*(const BigInteger &other) const {
-  //+...+ or .... ?
+
+BigInteger BigInteger::operator*(const BigInteger &other) const {
+  BigInteger new_number;
+  vector<char> tmp_number;
+  char remainder = 0;
+  int first, second;
+  first = second = 0;
+  for (size_t i = 0; i < other.number.size(); i++) {
+    first = other.number[i];
+    for (size_t j = 0; j < i; j++) {
+      tmp_number.push_back(0);
+    }
+    remainder = 0;
+    for (size_t j = 0; j < number.size(); j++) {
+      second = number[j];
+      tmp_number.push_back((first * second + remainder) % kDimension);
+      remainder = (first * second + remainder) / kDimension;
+    }
+    if (remainder != 0) {
+      tmp_number.push_back(remainder);
+    }
+    new_number += BigInteger(tmp_number, true);
+    tmp_number.resize(0);
+  }
+  new_number.is_positive = is_positive == other.is_positive;
+  return new_number;
 }
-const BigInteger BigInteger::operator+=(const BigInteger &other) const {
-  return *this + other;
+
+BigInteger BigInteger::operator+=(const BigInteger &other) {
+  *this = *this + other;
+  return *this;
 }
-const BigInteger BigInteger::operator-=(const BigInteger &other) const {
-  return *this - other;
+
+BigInteger BigInteger::operator-=(const BigInteger &other) {
+  *this = *this - other;
+  return *this;
 }
-const BigInteger BigInteger::operator*=(const BigInteger &other) const {
-  return *this*other;
+
+BigInteger BigInteger::operator*=(const BigInteger &other) {
+  *this = *this * other;
+  return *this;
 }
-const BigInteger BigInteger::operator-(void) const {
+
+BigInteger BigInteger::operator/=(const BigInteger &other) {
+  *this = *this / other;
+  return *this;
+}
+
+BigInteger BigInteger::operator%=(const BigInteger &other) {
+  *this = *this % other;
+  return *this;
+}
+
+BigInteger BigInteger::operator-() const {
   return BigInteger(number, !is_positive);
 }
-const BigInteger BigInteger::operator++(void) {
+
+BigInteger& BigInteger::operator++() {
   *this = *this + 1;
   return *this;
 }
-const BigInteger BigInteger::operator++(int) {
+
+BigInteger BigInteger::operator++(int) {
   BigInteger old_int = *this;
   ++*this;
   return old_int;
 }
-const BigInteger BigInteger::operator--(void) {
+
+BigInteger& BigInteger::operator--() {
   *this = *this - 1;
   return *this;
 }
-const BigInteger BigInteger::operator--(int) {
+
+BigInteger BigInteger::operator--(int) {
   BigInteger old_int = *this;
   --*this;
   return old_int;
 }
-const BigInteger BigInteger::operator+(int other) const {
-  vector<char> array = number;
-  array[0] += other;
-  return BigInteger(array, is_positive);
+
+vector<char> Reverse(vector<char> vec) {
+  char tmp = 0;
+  for (size_t i = 0; i < vec.size()/2; i++) {
+    tmp = vec[i];
+    vec[i] = vec[vec.size() - i - 1];
+    vec[vec.size() - i - 1] = tmp;
+  }
+  return vec;
 }
 
+BigInteger BigInteger::operator/(BigInteger other) const {
+  //TODO: do smth with 0
+  if (other == 0) {
+    return {0}; //wtf
+  }
+  bool is_other_positive = other.is_positive;
+  other.is_positive = true;
+  vector<char> reversed_result;
+  vector<char> tmp_number;
+  vector<char> reversed_number = Reverse(number);
+  for (char ch : reversed_number) {
+    tmp_number.push_back(ch);
+    BigInteger tmp_int(Reverse(tmp_number), true);
+    if (tmp_int < other) {
+      continue;
+    }
+    reversed_result.push_back(tmp_int.small_division(other));
+    tmp_number = Reverse(tmp_int.number);
+    if (tmp_number.size() == 1 && tmp_number[0] == 0) {
+      tmp_number.resize(0);
+    }
+  }
+  if (reversed_result.empty()) {
+    reversed_result.push_back(0);
+  }
+  return BigInteger(Reverse(reversed_result), is_positive == is_other_positive);
+}
+
+int BigInteger::small_division(const BigInteger &other) {
+  int count = 0;
+  while (*this >= other) {
+    count++;
+    *this -= other;
+  }
+  return count;
+}
+
+BigInteger BigInteger::operator%(const BigInteger &other) const {
+  BigInteger div = *this / other;
+  return *this - div * other;
+}
+
+bool BigInteger::operator==(const BigInteger &other) const {
+  if (other.number.size() != number.size()) {
+    return false;
+  }
+  if (number.size() == 1 && number[0] == 0 && other.number[0] == 0) {
+    return true;
+  }
+  if (is_positive != other.is_positive) {
+    return false;
+  }
+  for (size_t i = 0; i < number.size(); i++) {
+    if (number[i] != other.number[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool BigInteger::operator!=(const BigInteger &other) const {
+  return !(*this == other);
+}
+
+bool BigInteger::operator<(const BigInteger &other) const {
+  if (other.number.size() > number.size()) {
+    return true;
+  }
+  if (other.number.size() < number.size()) {
+    return false;
+  }
+  for (int i = number.size() - 1; i >= 0; i--) {
+    if (number[i] < other.number[i]) {
+      return true;
+    } else if (number[i] > other.number[i]) {
+      return false;
+    }
+  }
+  return false;
+}
+
+bool BigInteger::operator<=(const BigInteger &other) const {
+  return *this < other || *this == other;
+}
+
+bool BigInteger::operator>(const BigInteger &other) const {
+  return other < *this;
+}
+
+bool BigInteger::operator>=(const BigInteger &other) const {
+  return other <= *this;
+}
+
+ostream& operator<<(ostream &os, const BigInteger &big_int) {
+  os << big_int.toString();
+  return os;
+}
+istream& operator>>(istream &is, BigInteger &big_int) {
+  string input;
+  is >> input;
+  vector<char> result;
+  for (auto i = input.rbegin(); i != input.rend(); i++) {
+    result.push_back(*i - '0');
+  }
+  big_int.number = result;
+  return is;
+}
+
+string BigInteger::toString() const {
+  if (number.size() == 1 && number[0] == 0) {
+    return "0";
+  }
+  vector<char> result;
+  for (auto i = number.rbegin(); i != number.rend(); i++) {
+    result.push_back(*i + '0');
+  }
+  string str = {result.begin(), result.end()};
+  if (!is_positive) {
+    return "-" + str;
+  }
+  return str;
+}
+
+BigInteger::operator bool() const {
+  return !(number.empty() || (number.size() == 1 && number[0] == 0));
+}
+
+BigInteger operator-(int first, const BigInteger &second) {
+  return BigInteger(first) - second;
+}
+
+BigInteger operator+(int first, const BigInteger &second) {
+  return BigInteger(first) + second;
+}
+
+BigInteger operator*(int first, const BigInteger &second) {
+  return BigInteger(first) * second;
+}
+
+BigInteger operator/(int first, const BigInteger &second) {
+  return BigInteger(first) / second;
+}
+
+BigInteger operator%(int first, const BigInteger &second) {
+  return BigInteger(first) % second;
+}
+/*
+BigInteger &operator%=(const BigInteger &first, const BigInteger &second) {
+  BigInteger& first_1 = const_cast<BigInteger&>(first);
+  first_1 = first_1 % second;
+  return first_1;
+}
+*/
 #endif //FIVT_BIGINTEGER_H
--- task-4-A/main.cpp
+++ task-4-A/main.cpp
@@ -6,10 +6,33 @@
 #include "biginteger.h"
 
 int main() {
-  vector<char> v = {0};
-  BigInteger i(v, true);
-  BigInteger j = i++;
-  cout << (int)i.number[0] << endl;
-  cout << (int)j.number[0] << endl;
+  int ii = -12;
+  int jj = -5;
+  BigInteger i(ii);
+  BigInteger j(jj);
+  //cin >> j;
+  cout << i << endl;
+  cout << j << endl;
+  i %= i + 1;
+  ii %= ii + 1;
+  cout << i << endl;
+  cout << ii << endl;
+
+  int aa, bb;
+  aa = 302696;
+  bb = -1066991;
+  BigInteger a(aa);
+  BigInteger b(bb);
+  cin >> a;
+  std::string testString = a.toString() + " " + (-b).toString();
+  cout << testString << endl;
+  b += ++a %= a /= a *= a -= b++;
+  bb += ++aa %= aa /= aa *= aa -= bb++;
+  cout << a << " " << b << endl;
+  cout << aa << " " << bb << endl;
+  cout << 5+a << ' ' << 1-b << endl; // 5 -1000
+  cout << 5+aa << ' ' << 1-bb << endl; // 5 -1000
+  cout << (b == a + 5);
+
   return 0;
 }
